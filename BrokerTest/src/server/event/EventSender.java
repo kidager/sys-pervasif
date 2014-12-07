4ba2bade339431df95c5a99ca012fcef68355287
Jacem Chaieb <jacem@chaieb.tn>
2014-12-07 14:03:20 +0100

    Signed-off-by: Jacem Chaieb <jacem@chaieb.tn>

--- BrokerTest/src/server/event/EventSender.java
+++ BrokerTest/src/server/event/EventSender.java
@@ -1,5 +1,33 @@
 package server.event;
 
-public class EventSender {
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class EventSender implements Runnable {
+
+  private String sendToAddress = "0.0.0.0";
+  private int    sendToPort    = 0;
+  private Event  eventToSend;
+
+  public EventSender(String sendToAddress, int sendToPort, Event eventToSend) {
+    this.sendToAddress = sendToAddress;
+    this.sendToPort = sendToPort;
+    this.eventToSend = eventToSend;
+  }
+
+  @Override
+  public void run() {
+    try {
+      Socket socket = new Socket(sendToAddress, sendToPort);
+      ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+      oos.writeObject(eventToSend);
+      oos.flush();
+      oos.close();
+      socket.close();
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+  }
 
 }
